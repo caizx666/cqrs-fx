93c45e881afef0d8bbec5fbb24c463ffcf7794a3
caizhenxing <caizhenxing@gmail.com>
2017-01-04 17:55:31 +0800

    微调

--- src/index.js
+++ src/index.js
@@ -3,15 +3,8 @@ import commandhandler from './command/handler';
 import eventhandler from './event/handler';
 import aggregate from './aggregate';
 import app from './app';
-import {alias,_require} from './core';
-import {register,registry} from './register';
 
 export default {
-  alias,
-  require:_require,
-  register,
-  registry,
-
   app,
   bus,
   aggregate,
