079a3b3c0d86e42848e48eedcc1c4057c8313e82
caizx <caizx@chanjet.com>
2017-05-31 10:10:11 +0800

    修改handler注册

--- src/utils.js
+++ src/utils.js
@@ -178,7 +178,7 @@ export function merge(...args) {
 
 export function getClassName(Type) {
   assert(Type);
-  if (Type.name != '_class') {
+  if (Type.name != '_class' && Type.name != '_default') {
     return Type.name;
   }
   const filename = Type.prototype.__filename;
